6b97cced683be8712eee9c1cbe04e1fd7137d2d7
sharonOliva <olivasharon1819@gmail.com>
2023-04-10 16:52:37 -0300

    login fixes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -34,13 +34,11 @@ const Login = () => {
       const data = request.data
       console.log(data)
       if (data.data.token != null) {
-        Swal.fire("logueado")
-      } else {
-        Swal.fire("no logueado")
-      }
+        Swal.fire("Logueado")
+      } 
     } catch (error) {
       console.log(error)
-      return Swal.fire(error.msg)
+      return Swal.fire("No logueado")
 
     }
   }
@@ -56,7 +54,7 @@ const Login = () => {
 
                     <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Login</p>
 
-                    <form className="mx-1 mx-md-4" onClick={onhandleSubmit}>
+                    <form className="mx-1 mx-md-4">
 
 
                       <div className="d-flex flex-row align-items-center mb-4">
@@ -75,7 +73,7 @@ const Login = () => {
                         </div>
                       </div>
                       <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button type="button" className="btn btn-primary btn-lg" >Iniciar sesión</button>
+                        <button type="button" className="btn btn-primary btn-lg" onClick={onhandleSubmit} >Iniciar sesión</button>
                       </div>
                     </form>
                   </div>
